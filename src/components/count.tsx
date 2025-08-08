0167a532ddb7c61d3bf8373aa4b250a0d2497afa
L1nexx <ellkaellka1337@gmail.com>
2025-08-08 19:43:08 +0300

    Update App.tsx and count component

--- src/components/count.tsx
+++ src/components/count.tsx
@@ -1,1 +1,16 @@
 import { useEffect, useState} from 'react'
+
+
+export default function count() {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        document.title = `count: ${count}`
+    }, [count])
+
+    return(
+        <>
+            <button onClick={() => setCount((count) => count + 1)}></button>
+            <h1>Count: {count}</h1>
+        </>
+    )
+}
